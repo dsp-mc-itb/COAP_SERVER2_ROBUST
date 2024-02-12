0531cef5c117462e1761195c08d476f70b190071
seanmantanhacker <pribadi.sean@gmail.com>
2024-02-12 14:18:54 +0700

    a

--- src/coap-server.c
+++ src/coap-server.c
@@ -484,6 +484,7 @@ hnd_get_example_data(coap_resource_t *resource,
     }
     example_data_value = alloc_resource_data(value);
   }
+  printf("TES SEAN\n");
   coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
   body = reference_resource_data(example_data_value);
   coap_add_data_large_response(resource, session, request, response,
